9de8a63d3ec320b5d06fa279a09ba75aea2270ea
Om Jadhav <omjadhav9271@gmail.com>
2025-03-24 19:25:36 +0530

    8:54

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+export const mongoDBURL='mongodb+srv://omjadhav9271:EzuzVGaYCXJ7PyvU@bookstore.lc3i2.mongodb.net/?retryWrites=true&w=majority&appName=BookStore';
+// The PORT constant is exported to make it accessible to other files. This allows the value of the PORT constant to be used in other files by importing it using the import statement.
+// EzuzVGaYCXJ7PyvU
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from './config.js';
+import { PORT,mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 app.listen(PORT, () => {
@@ -7,8 +8,16 @@ app.listen(PORT, () => {
 });
 // The import statement is used to import the PORT constant from the config.js file. The PORT constant is then used in the app.listen() method to specify the port on which the server should listen. This allows the port number to be easily changed by updating the value in the config.js file.
 
-app.get("/", (request, response) => {
-  console.log(request);
-  return response.status(234).send("Server Responding: Hello World!");
+mongoose.connect(mongoDBURL)
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.get("/", (request, response) => {
+    console.log(request);
+    return response.status(234).send("Server Responding: Hello World!");
+  });
+  // The app.get() method is used to define a route for the root URL ("/") that sends a response of "Hello World!" when the route is accessed. This route will be accessible at the root URL of the server (e.g., http://localhost:5555/).
+  
+})
+.catch((error) => {
+  console.log('Error connecting to MongoDB:', error);
 });
-// The app.get() method is used to define a route for the root URL ("/") that sends a response of "Hello World!" when the route is accessed. This route will be accessible at the root URL of the server (e.g., http://localhost:5555/).
